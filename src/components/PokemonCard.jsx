1075748e26e8e42002d519d17a660d1ac9390893
MOHAMED MAACH <maach.med@yahoo.com>
2023-03-28 11:31:59 +0200

    Mew saffiche pas sucess

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,34 @@
+
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 function PokemonCard() {
+const pokemon=pokemonList[1];
+if(pokemon.imgSrc!==undefined) {
     return <figure>
-    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bulbasaur pokemon"/>
-    <figcaption>bulbasaur</figcaption>
-   </figure>;
-  }
   
+    <img src={pokemon.imgSrc} alt={pokemon.name}/>
+    <figcaption> {pokemon.name} </figcaption>
+   </figure>;
+}
+    
+ else {
+            return <figure>
+   
+    <p> ??? </p>
+    <figcaption> {pokemon.name} </figcaption>
+    </figure>;
+        
+ }
+   
+}
+  
+
   export default PokemonCard;
