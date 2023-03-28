3b3111307a3014edf20bc302c1ff826e2ef7b473
MOHAMED MAACH <maach.med@yahoo.com>
2023-03-28 10:22:45 +0200

    create a poke card

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import MyTitle from "./components/MyTitle"
+import PokemonCard from "./components/PokemonCard"
 
 function App() {
   return (
     <div>
-      <MyTitle />
+      <PokemonCard />
     </div>
   );
 }
