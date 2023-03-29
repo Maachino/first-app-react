3a2ef1ec287948e3e32045ea3602ef25a874e506
MOHAMED MAACH <maach.med@yahoo.com>
2023-03-29 11:40:15 +0200

    adding two buttons next and previous to switching in our Pokemon List

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,52 @@
 import MyTitle from "./components/MyTitle"
 import PokemonCard from "./components/PokemonCard"
+import { useState } from "react";
 
 function App() {
-  
-  
-  return (
-    <div>
-      <PokemonCard pokemon={pokemonList[0]} />
-    </div>
-  );
-}
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+  const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      },
+      {
+        name: "charmander",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+      },
+      {
+        name: "squirtle",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+      },
+      {
+        name: "pikachu",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+      },
+      {
+        name: "mew",
+      },
+    ];
+    const [count, setCount] = useState(0);
 
+    const next = () => {
+      if (count < pokemonList.length-1) { setCount(count + 1) }
+    }
+    const previous = () => {
+      if (count > 0) { setCount(count - 1) }
+    
+    }
+    
+    return (
+      <div>
+        <PokemonCard pokemon={pokemonList[count]} />
+        <button onClick={previous}>previous Pokemon</button>
+        <button onClick={next}>Next Pokemon</button>
+        <br/><p>´Pokemon number {count+1} Of {pokemonList.length}´</p>
+      </div>
+    );
+    }
+    
+  
 export default App;
