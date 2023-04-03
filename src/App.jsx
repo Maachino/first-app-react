dfaf06609ea416f41933fefe7788a699cbd9a65c
MOHAMED MAACH <maach.med@yahoo.com>
2023-04-03 10:04:07 +0200

    moving to NavBAR

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,51 @@
-import MyTitle from "./components/MyTitle"
-import PokemonCard from "./components/PokemonCard"
 import { useState } from "react";
 
-function App() {
-  const pokemonList = [
-    {
-        name: "bulbasaur",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-      },
-      {
-        name: "charmander",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
-      },
-      {
-        name: "squirtle",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
-      },
-      {
-        name: "pikachu",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-      },
-      {
-        name: "mew",
-      },
-    ];
-    const [count, setCount] = useState(0);
+import PokemonCard from "./components/PokemonCard";
 
-    const next = () => {
-      if (count < pokemonList.length-1) { setCount(count+1) }
-    }
-    const previous = () => {
-      if (count > 0) { setCount(count-1 )}
-    
-    }
-    
-    return (
-      <div>
-        <PokemonCard pokemon={pokemonList[count]} />
-                <button onClick={previous}>previous Pokemon</button>
-        <button onClick={next}>Next Pokemon</button>
-        <br/><p>Pokemon number {count+1} Of {pokemonList.length}</p>
-      </div>
-    );
-    }
-    
-  
+import "./App.css";
+import NavBar from "./components/NavBar";
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+function App() {
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  const pokemon = pokemonList[pokemonIndex];
+
+  return (
+    <div className="App">
+      <NavBar
+        pokemonIndex={pokemonIndex}
+        setPokemonIndex={setPokemonIndex}
+        pokemonList={pokemonList}
+      />
+      <PokemonCard pokemon={pokemon} />
+    </div>
+  );
+}
+
 export default App;
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,27 +1,23 @@
 import PropTypes from "prop-types";
 
-function PokemonCard(  {pokemon} ) {
+function PokemonCard({ pokemon }) {
+  return (
+    <figure>
+      {pokemon.imgSrc == null ? (
+        <p>???</p>
+      ) : (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      )}
+      <figcaption>{pokemon.name}</figcaption>
+    </figure>
+  );
+}
 
-if(pokemon.imgSrc!==undefined) { // can use another trick without if else butif also is basic
-    return <figure> 
-    <img src={pokemon.imgSrc} alt={pokemon.name}/>
-    <figcaption> {pokemon.name} </figcaption>
-   </figure>;
-}
- else {
-    return <figure>
-    <p> ??? </p>
-    <figcaption> {pokemon.name} </figcaption>
-    </figure>;     
- }
-   
-}
-PokemonCard.propTypes={
+PokemonCard.propTypes = {
   pokemon: PropTypes.shape({
     name: PropTypes.string.isRequired,
     imgSrc: PropTypes.string,
-}).isRequired,
-}
-  
-  export default PokemonCard;
+  }).isRequired,
+};
 
+export default PokemonCard;
